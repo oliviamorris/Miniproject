0854713d358175dc3f314e0b4fcd711f3eaaf101
Olivia Morris <olivia.morris15@imperial.ac.uk>
2019-11-25 16:31:17 +0000

    Edit to code

--- Code/HannahCodeOMcomments.R
+++ Code/HannahCodeOMcomments.R
@@ -12,7 +12,7 @@ setwd("Documents/PhD/Miniproject")
 #OM?i dont quite get where these paramters come from?+ why? i do a bit more now, theyre used in the builidng of the matrix but not hundy p, in order to know river phase time?, would chnage depending on number of stages you have inriver? and weve just said each one is a year, therefore six years?
 # Maximum number of full years spent in the river before leaving.
 # This includes all stages from egg to smolt (although smolt doesn't have an entry).
-Npy_V = 6
+Npy_V = 6 #OM:the way it is used it for defining where to put things essentially and used to predicting like probability of lh stuff 
 
 # Range size of parr years to allow e.g. Rpy = 2 if there is a main and alternative,
 # Rpy = 3 if there is one main LH chanel and two others that are important
@@ -27,7 +27,7 @@ NSW_V = 2
 # NSWR = 3 if there are 1SW, 2SW and 3SW females
 NSWR_V = 2
 
-#!!! Should do a check that NSWR<=NSW otherwise return an error #####?why?
+#!!! Should do a check that NSWR<=NSW otherwise return an error #?why?
 
 # Number of kelting events - i.e. how many repeat spawnings can take place after initial spawning (females only)
 Nk_V = 1 # CURRENTLY ONLY CHECKED FOR 0 AND 1
@@ -41,10 +41,10 @@ Nks_V = 1 # CHECK IF THIS CAN BE ZERO - i.e. they return after a few months at s
 NksR_V = 1
 
 # Parr survival 
-Spmat_V = matrix(0,Npy_V +1,1) # is this number of par stages based upon all stages from egg to smolt (i.e. Npy)
-Spmat_V[1] = 0.8608 # S_0p from model (use S_f from Maine system for now)
-Spmat_V[2] = 0.9458 # S_1p from model (use S_p from Maine system for now)
-Spmat_V[3] = 0.9458 # S_2p from model. Use Maine S_p for now. Needs to be recalculated for Iceland/Vesturdalsa
+Spmat_V = matrix(0,Npy_V +1,1) #OM: Sp is montly survival during 1-6 stages and freshwater smolt phase, so create a mtrix for this section? why seven? is it smolt too?
+Spmat_V[1] = 0.8608 # S_0p from model (use S_f from Maine system for now) #OM: Sf is monthly survival during fry stage
+Spmat_V[2] = 0.9458 # S_1p from model (use S_p from Maine system for now) #OM: sp is monthly survival suring entire par stages 1-6
+Spmat_V[3] = 0.9458 # S_2p from model. Use Maine S_p for now. Needs to be recalculated for Iceland/Vesturdalsa #OM these are the same values no matter the age(?can imporve this?)
 Spmat_V[4] = 0.9458 # S_3p from model. Use Maine S_p for now. Needs to be recalculated for Iceland/Vesturdalsa
 Spmat_V[5] = 0.9458 # S_4p from model. Use Maine S_p for now. Needs to be recalculated for Iceland/Vesturdalsa
 Spmat_V[6] = 0.9458 # S_5p from model. Use Maine S_p for now. Needs to be recalculated for Iceland/Vesturdalsa
@@ -52,15 +52,15 @@ Spmat_V[7] = 0.9458 # S_6p from model. Use Maine S_p for now. Needs to be recalc
 #Spmat_V[8] = 0.9458 # S_7p from model. Use Maine S_p for now. Needs to be recalculated for Iceland/Vesturdalsa
 Spmat_V #OM: view it
 
-Sps_V =  0.8721 # Monthly esturaine/initial marine survival i.e. survival of post-smolts
+Sps_V =  0.8721 # Monthly esturaine/initial marine survival #i.e. survival of post-smolts in marine phase
 # This is from overall estuary mortality of 56% in river Elidar in Iceland. I.e. 0.44^(1/6)
 
 
 pp1_V = 0.020 # The first entry of p_plus. The probability of smoltifying at the first opportunity.
 pp2_V = 0.526  # The second entry of p_plus. The probability of smoltifying at the second opportunity given that the
-# fish did not smoltify at the first opportunity.
+# fish did not smoltify at the first opportunity. #OM calculated in SI of asgard report taken from robertson?
 
-p_plus_V = matrix(0,Npy_V,1) #OM?same matrix produced as for smolts? why? cerates an empty m for something to do with smolting?
+p_plus_V = matrix(0,Npy_V,1) #OM?same matrix produced as for smolts? why? cerates an empty m for something to do with smolting? different life history pasths i think
 #p_plus_V[1] to p_plus_V[3] leave as 0 as can't smoltify at time t+1 if 0+, 1+ parr or 2+parr at time t (?is this iceland specific or fish specific?)
 p_plus_V[4] = pp1_V # The first non-zero entry of p_plus. The probability of smoltifying at the first opportunity.
 p_plus_V[5] = pp2_V # The second non-zero entry of p_plus. The probability of smoltifying at the second opportunity given that the
@@ -71,14 +71,14 @@ p_plus_V
 
 Sm_V = 0.97 #Monthly survival in the marine environment after first Jan 1st at sea (i.e. low mortality). Natural mortality. Value from Robertson (2005) based on ICES value
 Sr_V = 0.8 # Monthly survival in the river after returning to spawn. No information #?wtf look back at maine system?#
-#I DONT KNOW WHAT THESE ARE?   #?could be Appendix A2:  Calculation of survival probabilities across different life-stages ???
+#OM: I DONT KNOW WHAT THESE ARE?   #?could be Appendix A2:  Calculation of survival probabilities across different life-stages ???
 R1SW_V = 0.221
 R2SW_V = 1-R1SW_V   
-p1SW_V = ((R1SW_V/R2SW_V)*(0.97)^12) / (1+((R1SW_V/R2SW_V)*(0.97^12))) #huh 
+p1SW_V = ((R1SW_V/R2SW_V)*(0.97)^12) / (1+((R1SW_V/R2SW_V)*(0.97^12))) #OM:huh 
 
 p_SW_V = c(p1SW_V,1) # Made up for now. Entries are prob of female returning after 1SW, 
 # prob of female returning after 2SW given she didn't return after 1 SW
-Sk_V = 0.1 # A low value (guess - lower than Maine, USA) Overall survival as a kelt in the river - Low percentage (Thordardottir & Guðbergsson, 2017)
+Sk_V = 0.1 # A low value (guess - lower than Maine, USA) #Overall survival as a kelt in the river - Low percentage (Thordardottir & Guðbergsson, 2017)
 Se_V = 0.165 # Maine, USA for now   #Survival from egg to beginning of fry stage
 Sf_V = 0.8608 # Maine, USA for now  #Monthly survival during fry stage
 
@@ -99,8 +99,8 @@ testp_V = 2 #OM?what is this?
 #Set transition matrix to be correct size (initialise with zeros) #?what is a tranistion matrix?
 n_final = Npy_V + NSW_V + NSWR_V + Nk_V*(Nks_V + NksR_V) # Final dimension of matrix
 n = Npy_V + (2*NSW_V) + Nk_V*(Nks_V + NksR_V) # Intermediate dimension until "unreachable" stages deleted
-M = matrix(0,n,n) # creates blank matrix of n rows and n columns
-
+M = matrix(0,n,n) # creates blank matrix of n rows and n columns #OM based on number of states specified
+M
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Create transition matrix !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! (and the parr LH vector if required) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
@@ -122,7 +122,7 @@ for (i in 1:Npy_V-1){ #
 #OM: look at M now
 M
 
-# Last parr stage is special case (no stay-in-river option)
+# Last parr stage is special case (no stay-in-river option)#OM I.e you have to smolt?
 #OM: in m at row6+1,column6, insert the 6th row in pplus i.e probability of smolting is it? you have to at this point which is why one? times sixth row squared(why) in parr survival and seventh ^6(why) times post-smolt survival (esturay survival)
 M[Npy_V+1,Npy_V] = p_plus_V[Npy_V] * (Spmat_V[Npy_V]^2) * (Spmat_V[Npy_V+1]^6) * (Sps_V^4) # p_plus[Npy] == 1
 # Two months left as (Npy-1)+ parr (Npy th parr stage) in the river, and 6 months as Npy parr ((Npy+1)th parr stage) 
@@ -140,38 +140,43 @@ M
 
 # Post smolt is special case as has high initial marine mortality
 M[Npy_V+2, Npy_V+1] = p_SW_V[1]*(Sps_V^2) * (Sm_V^7) * (Sr_V^3) #OM:so at this time point[8,7]which is [1SWR,postsmolt] insert probability of female returning? times esturine survival to the two seven and three why?
-M[Npy_V+3, Npy_V+1] = (1-p_SW_V[1])*(Sps_V^2) * (Sm_V^10) #OM at nxt row down(1sw fish is it?^ is a returner appaz?)
+M[Npy_V+3, Npy_V+1] = (1-p_SW_V[1])*(Sps_V^2) * (Sm_V^10) #OM at nxt row down(1sw fish is it?^ is a returner appaz?)#this is 0ne minus(why) probablility of female returningafter 1sw times monthly marine surivial(sps) in esturay times monthly surivial at sea, why? how do you know this again
+M
 
 if (Nk_V == 1){ #OM: Nk is number of kelting events(repeat spwanings) # if there is only one
   M[Npy_V + (2*NSW_V) + 1, Npy_V + 2] = Sk_V * (Sm_V^5.5) # Returning 1SW fish becomes a post-kelt if kelting occurs
-} #OM check why this is doing this liv
+} #OM  at end of parr/smolt and at two times number of sea winters possible (stage11) and stage end of smolt plus two (8), transition is kelt surivial esturay survival  -wait is it cos it is 11 times 8 essentially?
+M #look
 
-M[1, Npy_V + 2] = Fec_V[1]*Se_V*(Sf_V^1.5)*(Spmat_V[1]^4) # Returning fish lays eggs, which survive 1.5 month as fry and 4 month 0 parr 
+M[1, Npy_V + 2] = Fec_V[1]*Se_V*(Sf_V^1.5)*(Spmat_V[1]^4) # Returning fish lays eggs, which survive 1.5 month as fry and 4 month 0 parr #OM <-?survival times
+M
 
 
 # Create rows and columns for all potential at sea and returner stages and delete those that don't occur later
 
-for (j in 2:(NSW)){
-  i = (j*2)
-  M[Npy + i, Npy + (i-1)] = p_SW[j] * Sm^9 * Sr^3 # Returns
+for (j in 2:(NSW_V)){ #no. sea winters
+  i = (j*2) 
+  M[Npy_V + i, Npy_V + (i-1)] = p_SW_V[j] * Sm_V^9 * Sr_V^3 # Returns #OM: prob of return * monthly surivival in marine * surviv in river once returned
   
-  if (j<NSW){      # The final returning fish option doesn't have a stay at sea option
-    M[Npy + i+1, Npy + (i-1)] = (1-p_SW[j]) * Sm^12 # Stays at sea
+  if (j<NSW_V){      # The final returning fish option doesn't have a stay at sea option
+    M[Npy_V + i+1, Npy_V + (i-1)] = (1-p_SW_V[j]) * Sm_V^12 # Stays at sea #OM: not sure
   }
-  if (Nk == 1){
-    M[Npy + (2*NSW) + 1, Npy + i] = Sk * (Sm^5.5) # Returning fish becomes a post-kelt if kelting occurs
+  if (Nk_V == 1){ #OM:If kelting is one (you spawned?)
+    M[Npy_V + (2*NSW_V) + 1, Npy_V + i] = Sk_V * (Sm_V^5.5) # Returning fish becomes a post-kelt if kelting occurs
   }
-  M[1, Npy + i] = Fec[j]*Se*(Sf^1.5)*(Spmat[1]^4) # Returning fish lays eggs
+  M[1, Npy_V + i] = Fec_V[j]*Se_V*(Sf_V^1.5)*(Spmat_V[1]^4) # Returning fish lays eggs
 }
 
-
+M
 #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
 
 #~~~~~~~~~~~~~~ Post kelts and repeat spawners ~~~~~~~~~~~~~~~~~~~~~~~~~~#
-if (Nk == 1){
-  M[Npy+(2*NSW)+2, Npy+(2*NSW)+1] = (Sm^9) * (Sr^3) # Post kelts at time t must become returners at t+1 (SIMPLIFYING ASSUMPTION)
-  M[1, Npy + (2*NSW)+2] = Fec[NSW+1]*Se*(Sf^1.5)*(Spmat[1]^4) # Returning fish lays eggs
+if (Nk_V == 1){ 
+  M[Npy_V+(2*NSW_V)+2, Npy_V+(2*NSW_V)+1] = (Sm_V^9) * (Sr_V^3) # Post kelts at time t must become returners at t+1 (SIMPLIFYING ASSUMPTION)
+  M[1, Npy_V + (2*NSW_V)+2] = Fec_V[NSW_V+1]*Se_V*(Sf_V^1.5)*(Spmat_V[1]^4) # Returning fish lays eggs
 }
+
+M
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
 
@@ -191,11 +196,46 @@ if (isitzero > 0){
   M = M[-unr,-unr]
 }
 
+#OM:dont have any in this example ^^
 
-#its called m not m_V i think#
-elast_a = elasticity(M_V)
-rowcol = which(elast_a!=0,arr.ind = T)
-e_values_a = elast_a[rowcol]
+#OM:its called m not m_V now 
+elast_a = elasticity(M) #OM: creates a matrix of elasticities of eigenvalues at each entry in matrix
 
-habitatcode = c(1,1,1,1,2,1,2,2,3,4,1,2,3,1,2,3,1)
-outy = cbind( e_values_a, habitatcode)
+rowcol = which(elast_a!=0,arr.ind = T) #OM:where are the results
+e_values_a = elast_a[rowcol] #OM: e values at those^points
+
+habitatcode = c(1,1,1,1,2,1,2,2,3,4,1,2,3,1,2,3,1) #OM:not sure who what or where this is
+outy = cbind( e_values_a, habitatcode) 
+
+outy2 = data.frame(outy)
+rows1 = which(outy2$habitatcode ==1) #OM:where is habitat code and rename
+rows2 = which(outy2$habitatcode ==2)
+rows3 = which(outy2$habitatcode ==3)
+rows4 = which(outy2$habitatcode ==4)
+
+sum(outy2[rows1,1]) #OM:values of all elasticities at those habitats
+sum(outy2[rows2,1])
+sum(outy2[rows3,1])
+sum(outy2[rows4,1])
+
+
+#######################################################################################################################
+mystuff = read.table("Data/Sensitivity_opt2_picklambda.csv", sep=",", header = T) #OM:wtf is this table
+cou=1 #OM: what is this
+M_Vlt = Vest_create_M(mystuff$Npy_V[cou], mystuff$Rpy_V[cou], mystuff$NSW_V[cou], 
+                      mystuff$NSWR_V[cou],mystuff$Nk_V[cou], mystuff$Nks_V[cou], mystuff$NksR_V[cou], 
+                      mystuff$Sps_V[cou], 
+                      rbind(0,0,0,mystuff$pp1_V[cou],mystuff$pp2_V[cou],1), 
+                      rbind(mystuff$Spmat_V1[cou],mystuff$Spmat_V2[cou],mystuff$Spmat_V3[cou],mystuff$Spmat_V4[cou],mystuff$Spmat_V5[cou],mystuff$Spmat_V6[cou],mystuff$Spmat_V7[cou]), 
+                      mystuff$Sm_V[cou], mystuff$Sr_V[cou], 
+                      rbind(mystuff$p1SW_V[cou],1), 
+                      mystuff$Sk_V[cou], mystuff$Se_V[cou], 
+                      mystuff$Sf_V[cou], 
+                      rbind(mystuff$Fec_V1[cou],mystuff$Fec_V2[cou],mystuff$Fec_V3[cou]), 
+                      mystuff$testp_V[cou])
+
+
+
+#OM - spmat v1 is different here compared to sensitivity data file...?
+#OM - fecV is also different?
+
