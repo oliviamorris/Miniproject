f08c8aa5d8e013b88d24d3bd6a1c7d4afb786158
Olivia Morris <olivia.morris15@imperial.ac.uk>
2019-11-28 11:52:05 +0000

    Code updates

--- Code/HannahCodeOMcomments.R
+++ Code/HannahCodeOMcomments.R
@@ -83,7 +83,7 @@ Se_V = 0.165 # Maine, USA for now   #Survival from egg to beginning of fry stage
 Sf_V = 0.8608 # Maine, USA for now  #Monthly survival during fry stage
 
 
-# Fecundity data
+# Fecundity data 
 Fec_V = matrix(0,3,1) # three by one matrix for fecundity
 Fec_V[1] = (2911/2) # Number of female eggs laid per 1SW female #Fecundity of female 1SW fish divided by two (i.e. number of female eggs) #?why? #Body size-fecundity relationship – 2kg fish (2016 statistics, Thordardottir & Guðbergsson, 2017)
 Fec_V[2] = (7532/2) # Number of female eggs laid per 2SW female #Fecundity of female 2SW fish divided by two (i.e. number of female eggs) #Body size-fecundity relationship – 5kg fish (2016 statistics, Thordardottir & Guðbergsson, 2017)
@@ -92,14 +92,14 @@ Fec_V
 
 #####################################################################################################################
 # Call the function to create the transition matrix etc.
-testp_V = 2 #OM?what is this?
+testp_V = 2 #OM?what is this? #uncommented and not made a difference
 #M_V = Vest_create_M(Npy_V, Rpy_V, NSW_V, NSWR_V, Nk_V, Nks_V, NksR_V, Sps_V, p_plus_V, Spmat_V, Sm_V, Sr_V, p_SW_V, Sk_V, Se_V, Sf_V, Fec_V, testp_V)
 
 
-#Set transition matrix to be correct size (initialise with zeros) #?what is a tranistion matrix?
-n_final = Npy_V + NSW_V + NSWR_V + Nk_V*(Nks_V + NksR_V) # Final dimension of matrix
+#Set transition matrix to be correct size (initialise with zeros) 
+n_final = Npy_V + NSW_V + NSWR_V + Nk_V*(Nks_V + NksR_V) # Final dimension of matrix # OM: based on these things - is this so it can be edited depending on your number of states etc, is this a gernalising thing
 n = Npy_V + (2*NSW_V) + Nk_V*(Nks_V + NksR_V) # Intermediate dimension until "unreachable" stages deleted
-M = matrix(0,n,n) # creates blank matrix of n rows and n columns #OM based on number of states specified
+M = matrix(0,n,n) # OM:creates blank matrix of n rows and n columns based on number of states specified
 M
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Create transition matrix !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
@@ -221,9 +221,24 @@ sum(outy2[rows4,1])
 
 #######################################################################################################################
 mystuff = read.table("Data/Sensitivity_opt2_picklambda.csv", sep=",", header = T) #OM:wtf is this table
-cou=1 #OM: what is this
+
+#OM: data contains the defined variables just in a table? - different values though for some not all?
+
+cou=1 #OM: what is this, is it for making a table for all rivers and yo uchoose which one or soemthing 
 M_Vlt = Vest_create_M(mystuff$Npy_V[cou], mystuff$Rpy_V[cou], mystuff$NSW_V[cou], 
                       mystuff$NSWR_V[cou],mystuff$Nk_V[cou], mystuff$Nks_V[cou], mystuff$NksR_V[cou], 
+                      mystuff$Sps_V[cou], 
+                      rbind(0,0,0,mystuff$pp1_V[cou],mystuff$pp2_V[cou],1), 
+                      rbind(mystuff$Spmat_V1[cou],mystuff$Spmat_V2[cou],mystuff$Spmat_V3[cou],mystuff$Spmat_V4[cou],mystuff$Spmat_V5[cou],mystuff$Spmat_V6[cou],mystuff$Spmat_V7[cou]), 
+                      mystuff$Sm_V[cou], mystuff$Sr_V[cou], 
+                      rbind(mystuff$p1SW_V[cou],1), 
+                      mystuff$Sk_V[cou], mystuff$Se_V[cou], 
+                      mystuff$Sf_V[cou], 
+                      rbind(mystuff$Fec_V1[cou],mystuff$Fec_V2[cou],mystuff$Fec_V3[cou]), 
+                      mystuff$testp_V[cou])
+
+M_Vlt = Vest_create_M(Npy_V, Rpy_V, NSW_V, 
+                      NSWR_V,Nk_V, Nks_V, mystuff$NksR_V[cou], 
                       mystuff$Sps_V[cou], 
                       rbind(0,0,0,mystuff$pp1_V[cou],mystuff$pp2_V[cou],1), 
                       rbind(mystuff$Spmat_V1[cou],mystuff$Spmat_V2[cou],mystuff$Spmat_V3[cou],mystuff$Spmat_V4[cou],mystuff$Spmat_V5[cou],mystuff$Spmat_V6[cou],mystuff$Spmat_V7[cou]), 
@@ -239,3 +254,292 @@ M_Vlt = Vest_create_M(mystuff$Npy_V[cou], mystuff$Rpy_V[cou], mystuff$NSW_V[cou]
 #OM - spmat v1 is different here compared to sensitivity data file...?
 #OM - fecV is also different?
 
+
+n_Vlt = Npy_V + (2*NSW_V) + Nk_V*(Nks_V + NksR_V)
+N1_Vlt = matrix(0,n_V,1)
+
+# Doesn't matter what this is
+N1_Vlt[1]= 117025
+N1_Vlt[2]= 49668
+N1_Vlt[3]= 8441
+N1_Vlt[4]= 6000
+N1_Vlt[5]= 3400
+N1_Vlt[6]= 1958
+N1_Vlt[7]= 819
+N1_Vlt[8]= 382
+N1_Vlt[9]= 13
+N1_Vlt[10]= 9
+N1_Vlt[11]= 6
+N1_Vlt[12]= 4
+
+maxit = 10000
+N_Vlt= matrix(0,n_V,maxit)
+N_Vlt[,1] = N1_Vlt
+
+for (i in 2:maxit){
+  N_Vlt[,i] = M_Vlt %*% N_Vlt[,i-1]
+  
+}
+
+prpss = matrix(0,n_V,maxit)
+sumss = colSums(N_Vlt)
+for (i in 1:maxit){
+  prpss[,i] = N_Vlt[,i]/sumss[i]
+}
+
+PRP = prpss[,maxit]
+PRP[8]+PRP[10]+PRP[12]
+#N1_Vbet = PRP*391/(PRP[8]+PRP[10]+PRP[12]) # For 1974
+
+#maxit = 20
+#N_Vltn= matrix(0,n_V,maxit)
+#N_Vltn[,1] = round(N1_Vbet)
+
+#for (i in 2:maxit){
+#	N_Vltn[,i] = M_V_base %*% N_Vlt[,i-1]
+
+#}
+
+catch_V = read.table("Data/Vesturdalsa_catch.txt",sep="")
+returners_n = N_Vltn[8,] + N_Vltn[10,] + N_Vltn[12,]
+#years2 = c(1974,1975,1976,1977,1978,1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993)
+
+#plot(years2[1:20],catch_V$V1[1:20],ylim=c(0,900),xlab="Year", ylab="Number of fish", col = "green", type="o")
+#lines(years2[1:20],returners_n,type="o", col="black")
+#legend(1970,-150,xpd=TRUE,legend=c("Model output - all returning females","Catch data"),col=c("black","green"), cex=0.8, pch=c(1,1) )
+
+###################################
+#############
+
+
+## Vesturdalsa better starting point from data and assumptions
+
+n_Vb = Npy_V + (2*NSW_V) + Nk_V*(Nks_V + NksR_V)
+N1_Vb = matrix(0,n_Vb,1)
+
+#N1_Vb[1]= 26238
+#N1_Vb[2]= 6997
+#N1_Vb[3]= 1749
+#N1_Vb[4]= 280
+#N1_Vb[5]= 32
+#N1_Vb[6]= 38
+#N1_Vb[7]= 520
+#N1_Vb[8]= 20
+#N1_Vb[9]= 355
+#N1_Vb[10]= 246
+#N1_Vb[11]= 3
+#N1_Vb[12]= 2
+
+
+N1_Vb[1]= 11284#7011#11504#23194/2 #91 #67938
+N1_Vb[2]= 16168#10046#16483#255134/2 #959 #747318
+N1_Vb[3]=  8556#5316#8722#111331/2 #411 # 326102
+N1_Vb[4]= 3006#1868#3065#74221/2 #274 # 217401
+N1_Vb[5]= 204#408#254#416#114
+N1_Vb[6]= 204#127#208
+N1_Vb[7]= 569#(150+169)/(Sm^10)#520
+N1_Vb[8]= 150#21
+N1_Vb[9]= 169#117//(Sm^12)#355
+N1_Vb[10]= 117#246
+N1_Vb[11]= 3
+N1_Vb[12]= 2
+
+
+maxit = 15
+N_Vb = matrix(0,n_Vb,maxit)
+N_Vb[,1] = N1_Vb
+
+for (i in 2:maxit){
+  N_Vb[,i] = M_V %*% N_Vb[,i-1]
+  
+}
+#catch_V = read.table("Vesturdalsa_catch.txt",sep="")
+
+returners = N_Vb[8,]+N_Vb[10,]+N_Vb[12,]
+years=c(2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016)
+plotout = cbind(returners,years,catch_V$V1[29:43])
+plot(plotout[,2], plotout[,1], type="o", col="black", xlab="Years", 
+     ylab = "Number", ylim=c(0,400))
+lines(plotout[,2],plotout[,3], type="o", col="purple")
+legend(1999, -70, xpd = TRUE, legend=c("Model - all female returners","Vesturdalsa catch data"), col=c("black","purple"),cex=0.8,pch=c(1,1,1))
+
+########## The long term proportions method of setting N1 ##############
+n_Vb3 = Npy_V + (2*NSW_V) + Nk_V*(Nks_V + NksR_V)
+N1_Vb3 = matrix(0,n_Vb3,1)
+
+# Long term proportion from model
+#Ltprp = propN_V_lt[,11] 
+
+for (i in 1:12){
+  N1_Vb3[i] = PRP[i] * 269/(PRP[8]+PRP[10]+PRP[12])
+}
+
+#for (i in 1:12){
+#	N1_Vb3[i] = Ltprp[i] * 269/(0.0054+0.0008+0.0006)
+#}
+
+N1_Vb3 = round(N1_Vb3)
+maxit = 15
+N_Vb3 = matrix(0,n_Vb3,maxit)
+N_Vb3[,1] = N1_Vb3
+
+for (i in 2:maxit){
+  N_Vb3[,i] = M_V %*% N_Vb3[,i-1]
+  
+}
+
+returners3 = N_Vb3[8,]+N_Vb3[10,]+N_Vb3[12,]
+years=c(2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016)
+plotout3 = cbind(returners3,years,catch_V$V1[29:43])
+plot(plotout3[,2], plotout3[,1], type="o", col="black", xlab="Year", 
+     ylab = "Number", ylim=c(0,350))
+lines(plotout3[,2],plotout3[,3], type="o", col="purple")
+legend(1999, -55, xpd = TRUE, legend=c("Model - all female returners","Vesturdalsa catch data"), col=c("black","purple"),cex=0.8,pch=c(1,1,1))
+
+plotout3 = cbind(returners3,years,catch_V$V1[29:43])
+yearsE = c(2002,2004,2006,2008,2010,2012,2014,2016)
+
+# Composite figure
+par(mfrow=c(2,1))
+par(mai=c(0.4,0.8,0.4,0.1))
+plot(plotout3[,2], plotout3[,1], type="o", col="black", xlab="", xaxt='n', 
+     ylab = "Number", ylim=c(0,350))
+axis(side = 1, at = yearsE, labels = FALSE, tck = -0.05)
+lines(plotout3[,2],plotout3[,3], type="o", col="purple")
+par(mai=c(0.9,0.8,0,0.1))
+plot(plotout[,2], plotout[,1], type="o", col="black", xlab="Year", 
+     ylab = "Number", ylim=c(0,350))
+lines(plotout[,2],plotout[,3], type="o", col="purple")
+legend(1999, -94, xpd = TRUE, bty = "n", legend=c("Model - all female returners","Vesturdalsa catch data"), col=c("black","purple"),cex=0.8,pch=c(1,1,1))
+
+
+
+
+#NAO = read.table("NAO.csv", sep=",", header=FALSE)
+
+#par(mfrow=c(2,1))
+#par(mai=c(0.1,0.8,0.5,0.1))
+#plot(plotout3[,2], plotout3[,1], type="o", col="black", xlab="Year", 
+#	ylab = "Number", ylim=c(0,350))
+#lines(plotout3[,2],plotout3[,3], type="o", col="green")
+#legend("topleft", legend=c("Model - all female returners","Vesturdalsa catch data"), col=c("black","green"),cex=0.8,pch=c(1,1,1))
+#barplot(NAO$V2, col="black", ylab = "NAO")
+
+Mack = read.table("Makrel.csv",header=FALSE)
+
+par(mfrow=c(4,1))
+par(mai=c(0.1,0.6,0.1,0.1))
+plot(plotout3[,2], plotout3[,1], type="o", col="black", xlab="Year", 
+     ylab = "Number", ylim=c(0,350))
+lines(plotout3[,2],plotout3[,3], type="o", col="green")
+legend("topleft", legend=c("Model - all female returners","Vesturdalsa catch data"), col=c("black","green"),cex=0.8,pch=c(1,1,1))
+par(mai=c(0.1,0.6,0.2,0.1))
+plot(plotout[,2], plotout[,1], type="o", col="black", xlab="Year",  
+     ylab = "Number", ylim=c(0,350))
+lines(plotout[,2],plotout[,3], type="o", col="green")
+legend("topleft", legend=c("Model - all female returners","Vesturdalsa catch data"), col=c("black","green"),cex=0.8,pch=c(1,1,1))
+plot(years,Mack$V1,type="o", col="blue", xlab="Years", ylab="Mackerel catch")
+par(mai=c(0.1,0.6,0.1,0.1))
+barplot(NAO$V2, col="black", ylab = "NAO")
+
+
+
+par(mfrow=c(4,1))
+par(mai=c(0.1,0.6,0.4,0.1))
+plot(plotout3[,2], plotout3[,1], type="o", col="black", xlab="Year", 
+     ylab = "Number", ylim=c(0,350))
+lines(plotout3[,2],plotout3[,3], type="o", col="green")
+legend("topleft", legend=c("Model - all female returners","Vesturdalsa catch data"), col=c("black","green"),cex=0.8,pch=c(1,1,1))
+plot(years,Mack$V1,type="o", col="blue", xlab="Years", ylab="Mackerel catch")
+barplot(NAO$V2, col="black", ylab = "NAO")
+
+
+compareN1s = cbind(N1_Vb, N1_Vb3)
+
+write.csv(propN_V_lt[,11],"model_longterm_prp.csv")
+
+
+########## The long term proportions method of setting N1 from 2003 ##############
+n_Vb4 = Npy_V + (2*NSW_V) + Nk_V*(Nks_V + NksR_V)
+N1_Vb4 = matrix(0,n_Vb4,1)
+
+# Long term proportion from model
+#Ltprp = propN_V_lt[,11] 
+
+for (i in 1:12){
+  N1_Vb4[i] = PRP[i] * 175/(PRP[8]+PRP[10]+PRP[12])
+}
+
+#for (i in 1:12){
+#	N1_Vb3[i] = Ltprp[i] * 175/(0.0054+0.0008+0.0006)
+#}
+
+N1_Vb4 = round(N1_Vb4)
+maxit = 14
+N_Vb4 = matrix(0,n_Vb4,maxit)
+N_Vb4[,1] = N1_Vb4
+
+for (i in 2:maxit){
+  N_Vb4[,i] = M_V %*% N_Vb4[,i-1]
+  
+}
+
+returners4 = N_Vb4[8,]+N_Vb4[10,]+N_Vb4[12,]
+years=c(2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016)
+plotout4 = cbind(returners4,years,catch_V$V1[30:43])
+plot(plotout4[,2], plotout4[,1], type="o", col="black", xlab="Year", 
+     ylab = "Number", ylim=c(0,350))
+lines(plotout4[,2],plotout4[,3], type="o", col="purple")
+legend(1999, -55, xpd = TRUE, legend=c("Model - all female returners","Vesturdalsa catch data"), col=c("black","purple"),cex=0.8,pch=c(1,1,1))
+
+
+## Vesturdalsa better starting point from data and assumptions 2003
+
+n_Vb = Npy_V + (2*NSW_V) + Nk_V*(Nks_V + NksR_V)
+N1_Vb = matrix(0,n_Vb,1)
+
+#N1_Vb[1]= 26238
+#N1_Vb[2]= 6997
+#N1_Vb[3]= 1749
+#N1_Vb[4]= 280
+#N1_Vb[5]= 32
+#N1_Vb[6]= 38
+#N1_Vb[7]= 520
+#N1_Vb[8]= 20
+#N1_Vb[9]= 355
+#N1_Vb[10]= 246
+#N1_Vb[11]= 3
+#N1_Vb[12]= 2
+
+
+N1_Vb[1]= 46322
+N1_Vb[2]= 12353
+N1_Vb[3]= 3088
+N1_Vb[4]= 494
+N1_Vb[5]= 82
+N1_Vb[6]= 41
+N1_Vb[7]= 663
+N1_Vb[8]= 13
+N1_Vb[9]= 230
+N1_Vb[10]= 160
+N1_Vb[11]= 3
+N1_Vb[12]= 2
+
+
+maxit = 14
+N_Vb = matrix(0,n_Vb,maxit)
+N_Vb[,1] = N1_Vb
+
+for (i in 2:maxit){
+  N_Vb[,i] = M_V %*% N_Vb[,i-1]
+  
+}
+#catch_V = read.table("Vesturdalsa_catch.txt",sep="")
+
+returners = N_Vb[8,]+N_Vb[10,]+N_Vb[12,]
+years=c( 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016)
+plotout = cbind(returners,years,catch_V$V1[30:43])
+plot(plotout[,2], plotout[,1], type="o", col="black", xlab="Years", 
+     ylab = "Number", ylim=c(0,350))
+lines(plotout[,2],plotout[,3], type="o", col="purple")
+legend(1999, -55, xpd = TRUE, legend=c("Model - all female returners","Vesturdalsa catch data"), col=c("black","purple"),cex=0.8,pch=c(1,1,1))
